5b32467aa9606a14fa2049ec24fb8f527f8971d9
Tanguy Ortolo <tanguy+debian@ortolo.eu>
2016-08-30 15:18:29 +0200

    Remove unused dh_make templates

--- debian/aiodns.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the aiodns package
-#
-0 4	* * *	root	[ -x /usr/bin/aiodns_maintenance ] && /usr/bin/aiodns_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(aiodns):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="aiodns" command="/usr/bin/aiodns"
